6b550a2c9982d638b33c17f0471d1e3d8018a109
Saptarshi Basu <123saptarshi.basu@gmail.com>
2021-10-04 11:38:43 +0530

    fixed warnings

--- src/components/Cards/HostCard.component.jsx
+++ src/components/Cards/HostCard.component.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Flex, Avatar, Box, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { supabase } from "../../Helpers/supabase";
